05dc3b4f31edb1d6cbe76040dfc89cc5e8698b3b
Brooke Heck <b19heck@gmail.com>
2022-09-19 23:14:27 +0000

    add an index.js file

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const PORT = process.env.PORT || 3002;
-
 app.get('/', (request, response) => {
   try {
     response.status(200).send('Development Practice Server');
