0d38c7c4eacbb80845405ac4a31c9d0c50fa14ff
Brooke Heck <b19heck@gmail.com>
2022-09-19 22:27:46 +0000

    add a get request that adds numbers

--- server.js
+++ server.js
@@ -10,11 +10,28 @@ const PORT = process.env.PORT || 3002;
 
 app.get('/', (request, response) => {
   response.status(200).send('Development Practice Server');
-})
+});
+
+app.get('/add/:num1/:num2', (request, response) => {
+  const sum = Number.parseInt(request.params.num1) + Number.parseInt(request.params.num2);
+  try {
+    response.status(200).send({sum: sum});
+  } catch(e) {
+    console.log(e);
+  }
+});
+
+app.get('/hello', (request, response) => {
+  try{
+    response.status(200).send({greeting: `Hello ${request.query.name}`});
+  } catch(e) {
+    console.log(e);
+  }
+});
 
 app.get('*', (request, response) => {
   response.status(404).send('No route');
-})
+});
 
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
